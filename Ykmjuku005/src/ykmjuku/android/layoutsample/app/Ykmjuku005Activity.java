bd9942fc2c6833033d4ad6d50194ae4a750b2e3c
yokmama <yokmama@kayosystem.com>
2012-02-29 11:56:35 +0900

    add Ykmjuku005

---
+++ Ykmjuku005/src/ykmjuku/android/layoutsample/app/Ykmjuku005Activity.java
@@ -0,0 +1,15 @@
+package ykmjuku.android.layoutsample.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Ykmjuku005Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gridlayout1);
+        
+    }
+}

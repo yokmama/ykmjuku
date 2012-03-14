301cec4cb75460e454f53561d5c3ff9619500961
yokmama <hijirinhijirin@gmail.com>
2012-03-14 10:25:48 +0900

    add Ykmjuku005

--- Ykmjuku005/gen/ykmjuku/android/layoutsample/app/R.java
+++ Ykmjuku005/gen/ykmjuku/android/layoutsample/app/R.java
@@ -17,38 +17,50 @@ public final class R {
     }
     public static final class id {
         public static final int button1=0x7f050001;
+        public static final int button10=0x7f05000e;
+        public static final int button11=0x7f05000f;
+        public static final int button12=0x7f050010;
+        public static final int button13=0x7f050012;
+        public static final int button14=0x7f050013;
+        public static final int button15=0x7f050014;
+        public static final int button16=0x7f050015;
         public static final int button2=0x7f050002;
         public static final int button3=0x7f050005;
         public static final int button4=0x7f050006;
-        public static final int button5=0x7f050007;
-        public static final int button6=0x7f05000d;
-        public static final int button7=0x7f05000e;
+        public static final int button5=0x7f050008;
+        public static final int button6=0x7f050009;
+        public static final int button7=0x7f05000a;
+        public static final int button8=0x7f05000b;
+        public static final int button9=0x7f05000d;
         public static final int imageView1=0x7f050000;
-        public static final int linearLayout1=0x7f050003;
-        public static final int linearLayout2=0x7f050004;
-        public static final int tableRow1=0x7f05000a;
-        public static final int tableRow2=0x7f05000b;
+        public static final int linearLayout1=0x7f050016;
+        public static final int linearLayout2=0x7f050017;
+        public static final int tableLayout1=0x7f050003;
+        public static final int tableRow1=0x7f050004;
+        public static final int tableRow2=0x7f050007;
         public static final int tableRow3=0x7f05000c;
-        public static final int textView1=0x7f050008;
-        public static final int textView2=0x7f050009;
+        public static final int tableRow4=0x7f050011;
+        public static final int textView1=0x7f050018;
+        public static final int textView2=0x7f050019;
     }
     public static final class layout {
         public static final int framelayout1=0x7f030000;
         public static final int gravity1=0x7f030001;
         public static final int gridlayout1=0x7f030002;
-        public static final int layout_gravity1=0x7f030003;
-        public static final int layout_weight=0x7f030004;
-        public static final int layout_width_height=0x7f030005;
-        public static final int linearlayout1=0x7f030006;
-        public static final int linearlayout2=0x7f030007;
-        public static final int linearlayout3=0x7f030008;
-        public static final int linearlayout4=0x7f030009;
-        public static final int main=0x7f03000a;
-        public static final int margin=0x7f03000b;
-        public static final int padding1=0x7f03000c;
-        public static final int padding2=0x7f03000d;
-        public static final int relativelayout1=0x7f03000e;
-        public static final int talbelayout1=0x7f03000f;
+        public static final int hoge=0x7f030003;
+        public static final int layout_gravity1=0x7f030004;
+        public static final int layout_weight=0x7f030005;
+        public static final int layout_width_height=0x7f030006;
+        public static final int linearlayout1=0x7f030007;
+        public static final int linearlayout2=0x7f030008;
+        public static final int linearlayout3=0x7f030009;
+        public static final int linearlayout4=0x7f03000a;
+        public static final int main=0x7f03000b;
+        public static final int margin=0x7f03000c;
+        public static final int padding1=0x7f03000d;
+        public static final int padding2=0x7f03000e;
+        public static final int relativelayout1=0x7f03000f;
+        public static final int talbelayout1=0x7f030010;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
